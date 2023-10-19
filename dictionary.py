893f106e692a1a872e1b401b08a54c5876274b07
programmer-88 <ianmbayyz@gmail.com>
2023-10-19 21:21:21 +0300

    improved translate function

--- dictionary.py
+++ dictionary.py
@@ -4,12 +4,12 @@ import json
 dictionary = json.load(open("data.json"))
 
 def translate(word):
-    return dictionary[word]
+    if word in dictionary:
+            return dictionary[word]
+
+    else:
+        print("{:s} is not a valid word !".format(word))
 
 word = input("Enter word: ")
 
-if word in dictionary:
-    print(str(translate(word)))
-
-else:
-    print("{:s} is not a valid word !".format(word))
+print(translate(word))
