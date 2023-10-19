69c9fa8b04a1eadef551139536e45e6e489ab75b
programmer-88 <ianmbayyz@gmail.com>
2023-10-19 21:57:27 +0300

    added a closely missplelt function

--- dictionary.py
+++ dictionary.py
@@ -1,20 +1,33 @@
 import json
-
+from difflib import get_close_matches
 
 dictionary = json.load(open("data.json"))
 
 # get answer from dictionary and give error if not found
 def translate(word):
+    word = word.lower()
     if word in dictionary:
             return dictionary[word]
 
     else:
-        print("{:s} is not a valid word. Please double check".format(word))
+        return get_closest(word)
+# chexk if there is a word that nearly matches
+def get_closest(word):
+    if len(get_close_matches(word, dictionary.keys())) > 0:
+        close_match = get_close_matches(word, dictionary.keys())[0]
+        confirmation = input(f"Did you mean {close_match} instead? Enter Y if yes or N if no: ")
 
-# convert word input to lowercase
-def to_lower(word):
-    return word.lower()
+        if confirmation.lower() == "y":
+            return dictionary[close_match]
+        
+        elif confirmation.lower() == "n":
+             return "{:s} does not exist. Please double check it.".format(word)
+        
+        else:
+             return "{:s} does not exist. Please double check it.".format(word)
 
+    else:
+         return "{:s} does not exist. Please double check it.".format(word)
 word = input("Enter word: ")
 
-print(translate(to_lower(word)))
+print(translate(word))
