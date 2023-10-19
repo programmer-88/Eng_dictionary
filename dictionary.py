806bdbdcaafab90ec4cf2c44f2a89d5d7fbe1981
programmer-88 <ianmbayyz@gmail.com>
2023-10-19 21:26:00 +0300

    considered uppercase string input

--- dictionary.py
+++ dictionary.py
@@ -3,13 +3,18 @@ import json
 
 dictionary = json.load(open("data.json"))
 
+# get answer from dictionary and give error if not found
 def translate(word):
     if word in dictionary:
             return dictionary[word]
 
     else:
-        print("{:s} is not a valid word !".format(word))
+        print("{:s} is not a valid word. Please double check".format(word))
+
+# convert word input to lowercase
+def to_lower(word):
+    return word.lower()
 
 word = input("Enter word: ")
 
-print(translate(word))
+print(translate(to_lower(word)))
